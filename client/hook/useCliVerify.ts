83c6a915476bcfa495358c26d1e922b6d52b3075
vanthaita <thaitv225@gmail.com>
2026-01-05 21:49:49 +0700

    chore(client): Add logging for useCliVerify mutation lifecycle

--- client/hook/useCliVerify.ts
+++ client/hook/useCliVerify.ts
@@ -5,5 +5,17 @@ import { AuthService } from '@/service/auth.service';
 export const useCliVerify = () => {
   return useMutation({
     mutationFn: async (userCode: string) => AuthService.cliVerify(userCode),
+    onMutate: (userCode: string) => {
+      // eslint-disable-next-line no-console
+      console.log('[useCliVerify] mutate', { userCode });
+    },
+    onSuccess: (data, userCode) => {
+      // eslint-disable-next-line no-console
+      console.log('[useCliVerify] success', { userCode, data });
+    },
+    onError: (error, userCode) => {
+      // eslint-disable-next-line no-console
+      console.log('[useCliVerify] error', { userCode, error });
+    },
   });
 };
